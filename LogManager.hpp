0e6ec47b813d04c004ecae0febd2770fdf9d2a09
jeekpark <jeekpark@student.42seoul.kr>
2024-02-04 18:59:52 +0900

    feat: LogManager

--- LogManager.hpp
+++ LogManager.hpp
@@ -17,6 +17,13 @@
 #include <string>
 #include <chrono>
 #include <iomanip>
+#include <fstream>
+#include <ctime>
+#include <sys/stat.h>
+
+#if !defined(__clang__) && !defined(__GNUC__)
+    #define __PRETTY_FUNCTION__ ("not supported")
+#endif
 
 class LogManager
 {
@@ -53,5 +60,42 @@ public:
         }
     }
 private:
+    std::ofstream mLogFile;
     std::mutex mu_;
+private:
+    LogManager()
+    {
+        createLogFolder();
+        std::ostringstream fileName;
+        fileName << "log/log_" << currentTimeStr() << ".txt";
+        mLogFile.open(fileName.str().c_str(), std::ios::app);
+    }
+    LogManager(const LogManager&);
+    LogManager& operator=(const LogManager&);
+    ~LogManager()
+    {
+        if (mLogFile.is_open())
+            mLogFile.close();
+    }
+
+    void createLogFolder()
+    {
+        struct stat st;
+        if (stat("log", &st) != 0)
+            mkdir("log", 0777);
+    }
+    
+    std::string currentTimeStr()
+    {
+        std::time_t now = std::time(0);
+        std::tm* localTime = std::localtime(&now);
+        std::ostringstream time;
+        time << (localTime->tm_year + 1900) << '-'
+                << (localTime->tm_mon + 1) << '-'
+                << localTime->tm_mday << '_'
+                << localTime->tm_hour << '-'
+                << localTime->tm_min << '-'
+                << localTime->tm_sec;
+        return time.str();
+    }
 };
