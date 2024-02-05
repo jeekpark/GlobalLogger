b92d05de7f267ee80fbb6cc7a0b5a9652ce5a843
jeekpark <jeekpark@student.42seoul.kr>
2024-02-05 11:36:54 +0900

    feat: 구조 작성

--- LogManager.hpp
+++ LogManager.hpp
@@ -25,15 +25,28 @@
     #define __PRETTY_FUNCTION__ ("not supported")
 #endif
 
+#define LOG_ERROR(msg)      LogManager::getInstance().log((msg), LogManager::ERROR, __FILE__, __LINE__, __PRETTY_FUNCTION__)
+#define LOG_WARNING(msg)    LogManager::getInstance().log((msg), LogManager::WARNING, __FILE__, __LINE__, __PRETTY_FUNCTION__)
+#define LOG_INFO(msg)       LogManager::getInstance().log((msg), LogManager::INFO, __FILE__, __LINE__, __PRETTY_FUNCTION__)
+#define LOG_DEBUG(msg)      LogManager::getInstance().log((msg), LogManager::DEBUG, __FILE__, __LINE__, __PRETTY_FUNCTION__)
+#define LOG_FATAL(msg)      LogManager::getInstance().log((msg), LogManager::FATAL, __FILE__, __LINE__, __PRETTY_FUNCTION__)
+
+/* #define PRINT_ERROR(msg)
+#define PRINT_WARNING(msg)
+#define PRINT_INFO(msg)
+#define PRINT_DEBUG(msg)
+#define PRINT_FATAL(msg) */
+
 class LogManager
 {
 public:
-    enum class LogLevel
+    enum LogLevel
     {
-        ERROR,
+        ERROR = 0,
         WARNING,
         INFO,
-        DEBUG
+        DEBUG,
+        FATAL
     };
 
     static LogManager& getInstance()
@@ -42,23 +55,32 @@ public:
         return instance;
     }
 
-    void log(const std::string& message, LogLevel level)
+    void log(const std::string& message, LogLevel level, const char *file, int line, const char *function)
     {
         std::lock_guard<std::mutex> lock(mu_);
         switch (level)
         {
-        case LogLevel::ERROR:
+        case ERROR:
+            mLogFile << formattedStr(currentTimeStr(), "ERROR", file, line, function, message);
             break;
-        case LogLevel::WARNING:
+        case WARNING:
+            mLogFile << formattedStr(currentTimeStr(), "WARN", file, line, function, message);
             break;
-        case LogLevel::INFO:
+        case INFO:
+            mLogFile << formattedStr(currentTimeStr(), "INFO", file, line, function, message);
             break;
-        case LogLevel::DEBUG:
+        case DEBUG:
+            mLogFile << formattedStr(currentTimeStr(), "DEBUG", file, line, function, message);
+            break;
+        case FATAL:
+            mLogFile << formattedStr(currentTimeStr(), "FATAL", file, line, function, message);
             break;
         default:
             break;
         }
     }
+
+
 private:
     std::ofstream mLogFile;
     std::mutex mu_;
@@ -91,11 +113,29 @@ private:
         std::tm* localTime = std::localtime(&now);
         std::ostringstream time;
         time << (localTime->tm_year + 1900) << '-'
-                << (localTime->tm_mon + 1) << '-'
-                << localTime->tm_mday << '_'
-                << localTime->tm_hour << '-'
-                << localTime->tm_min << '-'
-                << localTime->tm_sec;
+             << (localTime->tm_mon + 1) << '-'
+             << localTime->tm_mday << '_'
+             << localTime->tm_hour << '-'
+             << localTime->tm_min << '-'
+             << localTime->tm_sec;
         return time.str();
+    }
+
+    std::string formattedStr(const std::string& timeStamp, const std::string& level,
+                             const char* file, int line, const char* function,
+                             const std::string& message)
+    {
+        std::ostringstream res;
+
+        if (level == "DEBUG")
+        {
+            res << timeStamp << ": "
+                << file << ":" << line << ":" << function << ": ["
+                << level << "] " << message;
+        }
+        else
+            res << timeStamp << ": [" << level << "] " << message;
+        
+        return res.str();
     }
 };
---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include "LogManager.hpp"
+#include <iostream>
+int main()
+{
+    LOG_DEBUG("뭔가 문제 있음");
+    
+}
